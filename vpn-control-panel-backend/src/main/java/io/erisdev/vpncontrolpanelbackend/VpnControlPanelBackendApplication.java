88d6a13dad463a325be2c7feed3594d6aa506cf1
Haroun Ahmad <HarounAhmad02@gmail.com>
2025-07-16 11:45:15 +0200

    initial commit

---
+++ vpn-control-panel-backend/src/main/java/io/erisdev/vpncontrolpanelbackend/VpnControlPanelBackendApplication.java
@@ -0,0 +1,13 @@
+package io.erisdev.vpncontrolpanelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpnControlPanelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VpnControlPanelBackendApplication.class, args);
+    }
+
+}
---
+++ vpn-control-panel-backend/src/test/java/io/erisdev/vpncontrolpanelbackend/VpnControlPanelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.erisdev.vpncontrolpanelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpnControlPanelBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
